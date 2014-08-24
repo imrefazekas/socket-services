d07999194ff72fae7e5502845e96971df0f9a693
Imre Fazekas <imre.fazekas@icloud.com>
2014-08-24 12:14:56 +0200

    refinements

--- test/mochaTest.js
+++ test/mochaTest.js
@@ -2,10 +2,16 @@ var should = require("chai").should();
 
 var SocketServices = require('../lib/SocketServices');
 
-describe("socket-services", function () {
+var SocketClient = require('socket-services-client');
+
+describe("socket-services-client", function () {
 	var socketServices;
+	var client;
 	before(function(done){
-		socketServices = new SocketServices();
+		socketServices = new SocketServices( { port: 8080, ipAddress: '0.0.0.0', channel: 'api', event: 'rester', idLength: 16 } );
+		client = new SocketClient( {
+			host: 'http://localhost:8080', name: 'TestClient', channel: 'api', event:'rester', idLength: 16
+		} );
 
 		socketServices.connect( function(){
 			socketServices.publish( 'Tester', 'everything', function( data1, data2, callback ){
@@ -19,11 +25,17 @@ describe("socket-services", function () {
 
 	describe("socket", function () {
 		it('Services are', function(done){
-			done( );
+			client.connect( function(){
+				client.send( 'everything', 'How are you?', 'Everything is fine?', function(err, res){
+					console.log( 'Responded: ', err, res );
+					done( );
+				});
+			} );
 		});
 	});
 
 	after(function(done){
+		client.disconnect();
 		socketServices.close( function(){ console.log('Node stopped'); done(); } );
 	});
 });
